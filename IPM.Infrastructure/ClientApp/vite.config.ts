123b0c91e9ec1e51c3ed7267dffa8f9e81fc87b9
Nhat Tien <ledangnhattien04@gmail.com>
2024-11-17 20:05:34 +0700

    initial commit

---
+++ IPM.Infrastructure/ClientApp/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+  build: {
+    outDir: '../wwwroot/client-app/',
+    emptyOutDir: true, 
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  }
+})
---
+++ IPM.Infrastructure/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace IPM.Infrastructure.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
