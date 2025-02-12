2ee7f6ce8e8bb503001c94aa5d569a06b729198e
Nhat Tien <ledangnhattien04@gmail.com>
2025-02-12 22:04:33 +0700

    fix: fix cache fucntion

--- src/IPM.ClientApp/src/lib/stores/cache.svelte.ts
+++ src/IPM.ClientApp/src/lib/stores/cache.svelte.ts
@@ -15,7 +15,8 @@ function isValid(time: number | undefined, timeExpired: number | undefined): boo
 }
 
 export async function cacheFetch<T>(key: string, timeExpired: number, fetchCallback: () => Promise<T>): Promise<T> {
-  if(!cache.has(key) || !isValid(cache.get(key)?.time, cache.get(key)?.timeExpired)) {
+  //No cache
+  if(!cache.has(key)) {
 
     const data: T = await fetchCallback();
 
@@ -29,10 +30,32 @@ export async function cacheFetch<T>(key: string, timeExpired: number, fetchCallb
       resolve(data);
     })
   }
+  
+  // Has cache but timeout
+  if(!isValid(cache.get(key)?.time, cache.get(key)?.timeExpired)) {
+    cache.delete(key);
 
+    const data: T = await fetchCallback();
+
+    cache.set(key, {
+      time: Date.now(),
+      timeExpired: timeExpired,
+      data: data
+    });
+
+    return new Promise((resolve, _) => {
+      resolve(data);
+    })
+  }
+  
+  // Has cache
   const data = cache.get(key)?.data!;
 
   return new Promise((resolve, _) => {
     resolve(data);
   });
+}
+
+export const invalidateCache = (key: string) => {
+    cache.delete(key);
 }
